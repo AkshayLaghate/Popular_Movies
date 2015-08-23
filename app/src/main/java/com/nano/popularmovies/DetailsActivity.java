4c3157f87ef1a1598360ed598cc02dd3dbe90772
Akshay Laghate <akki.the.abe@gmail.com>
2015-08-23 16:21:29 +0530

    + Multipane Tablet Layout. - Strike Feature. + Removed extra stuff. + Trailer Sharing. + Final.

--- app/src/main/java/com/nano/popularmovies/DetailsActivity.java
+++ app/src/main/java/com/nano/popularmovies/DetailsActivity.java
@@ -351,8 +351,12 @@ public class DetailsActivity extends AppCompatActivity implements View.OnClickLi
                 Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
                 sharingIntent.setType("text/plain");
                 sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Check out this awesome trailer!");
-                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, "https://www.youtube.com/watch?v=" + videos.get(0).get(TAG_KEY));
-                startActivity(Intent.createChooser(sharingIntent, "Share via"));
+                if (videos.size() > 0) {
+                    sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, "https://www.youtube.com/watch?v=" + videos.get(0).get(TAG_KEY));
+                    startActivity(Intent.createChooser(sharingIntent, "Share via"));
+                } else
+                    Toast.makeText(this, "No video available to share!", Toast.LENGTH_SHORT).show();
+
                 break;
             default:
                 break;
